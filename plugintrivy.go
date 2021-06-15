a02be204f8d51ce6850bac70d5eeddc1fdf77fea
andylongstaffe <andylongstaffe@gmail.com>
2021-06-15 14:00:54 +0100

    CH-2008: Upgrade to ch plugin 1.0. Remove old local interface bits.

--- plugintrivy.go
+++ plugintrivy.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"compliance-hub-plugin-trivy/config"
-	"compliance-hub-plugin-trivy/internal/gRPC/basic/service"
 	"compliance-hub-plugin-trivy/lservice"
 	"fmt"
+	service "github.com/deliveryblueprints/chplugin-go/v1.0/service"
+	"github.com/deliveryblueprints/chplugin-service-go/plugin"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
 	"net"
 )
 
-//var TrivyRouter *httprouter.Router
-
 func getNetListener(address string, port uint) net.Listener {
+	log.Info().Msgf("Binding gRPC server on %s:%d", address, port)
 	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", address, port))
 	if err != nil {
 		log.Fatal().Msgf("failed to listen: %v", err)
@@ -29,22 +29,13 @@ func main() {
 	netListener := getNetListener(config.Config.GetString("server.address"), config.Config.GetUint("server.port"))
 	gRPCServer := grpc.NewServer()
 
-	manifestServiceImpl := lservice.NewManifestServiceGrpcImpl()
-	service.RegisterManifestServiceServer(gRPCServer, manifestServiceImpl)
+	chPluginServiceImpl := plugin.CHPluginServiceBuilder(lservice.NewTrivyScanner())
+	service.RegisterCHPluginServiceServer(gRPCServer, chPluginServiceImpl)
 
 	// start the server
+	log.Info()
 	if err := gRPCServer.Serve(netListener); err != nil {
 		log.Fatal().Msgf("failed to serve: %s", err)
 	}
-
-	/*
-		TrivyRouter = httprouter.New()
-
-		TrivyRouter.GET("/status", handlers.HandleGetStatus)
-
-		TrivyRouter.POST("/scan", handlers.HandlePostScan)
-
-		log.Fatal(http.ListenAndServe(":8080", TrivyRouter), nil)
-	*/
 
 }
